71b24adaab02a3cc5ef2caee80c91afeb47f3fa5
dav0ddevadze <xtinctruler@gmail.com>
2024-08-28 10:53:51 -0700

    'new'

---
+++ level-17/homework/folder.py
@@ -0,0 +1,22 @@
+
+x = 10
+while x <= 30:
+    print(x)
+    x += 1
+ 
+
+x = 50
+while x >= 10:
+    print(x)
+    x -= 1 
+
+
+
+
+
+   
+count = 0
+while count < 10:
+    print("goa best")
+    count += 1
+ 
---
+++ level-19/classwork/files.py
@@ -0,0 +1,23 @@
+num = int(input("Enter a number: "))
+
+if num > 0:
+    print("Positive number")
+elif num == 0:
+    print("Zero")
+else:
+    print("Negative number")
+
+
+age = 7
+
+if age < 18:
+    print("Underage")
+elif age == 18:
+    print("Teen")
+else:
+    print("Adult")
+
+
+
+
+
