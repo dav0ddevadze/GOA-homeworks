eba8890ba9ad15921091a3e4104d0858969b2284
dav0ddevadze <xtinctruler@gmail.com>
2024-09-18 12:53:14 -0700

    second commit

---
+++ level-19/homework/lvl.py
@@ -0,0 +1,15 @@
+current_hour = 10 
+if current_hour < 12:
+    print("Good morning!")
+else:
+    print("Good afternoon!")
+ temperature = 32  
+if temperature > 30:
+    print("It's hot outside!")
+else:
+    print("It's not too hot.")
+age = 16 
+if age < 19:
+    print("You are a teenager!")
+else:
+    print("You are not a teenager.")
