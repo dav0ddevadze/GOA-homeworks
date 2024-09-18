b3357784d466cd1a477e01debdc071aaea23ed3a
dav0ddevadze <xtinctruler@gmail.com>
2024-09-18 12:48:52 -0700

    second commit

---
+++ level-23/homework/kol0i.py
@@ -0,0 +1,17 @@
+
+list1 = ["apple", "banana", "cherry", "date"]
+print(list1[1])  
+
+
+list2 = ["apple", "banana", "cherry", "date"]
+list2[1] = "blueberry"
+print(list2) 
+
+
+
+
+list4 = ["apple", "banana", "cherry", "date", "elderberry"]
+print(list4[-5:-3]) 
+
+list5 = ["apple", "banana", "cherry", "date", "elderberry", "fig"]
+print([list5[0], list5[-3]])  
---
+++ level-25/classwork/folder.py
@@ -0,0 +1,6 @@
+name="david","alex","dani","levan","simon"
+print(name[2])
+print(name[3])
+
+num=1,2,3,4,5
+print(num[2])
