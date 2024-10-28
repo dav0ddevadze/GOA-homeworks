ecfb78a1c81171811bed84d3accd3b52bdc6b51d
dav0ddevadze <xtinctruler@gmail.com>
2024-10-28 06:40:06 -0700

    second commit

--- level-33/classwork/lvel33.py
+++ level-33/classwork/lvel33.py
@@ -1,3 +1,13 @@
-def celsius(cels):
 
-  return (cels+33.8)*5/9
+def celsius_to_fahrenheit(celsius):
+    fahrenheit = (celsius * 9/5) + 32
+    print(f"{celsius}°C is {fahrenheit}°F")
+
+def fahrenheit_to_celsius(fahrenheit):
+    celsius = (fahrenheit - 32) * 5/9
+    print(f"{fahrenheit}°F is {celsius}°C")
+
+
+celsius_to_fahrenheit(25)
+fahrenheit_to_celsius(77)
+
