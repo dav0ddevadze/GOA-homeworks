0f29e30dea64a1d1fe4868d6999a862bddbea5ed
unknown <xtinctruler@gmail.com>
2025-01-20 07:47:40 -0800

    second commit

---
+++ level-043/homework/homework.py
@@ -0,0 +1,11 @@
+list=[5,3,2,1,4]
+print(sum(list))
+
+list1=[5,3,2,1,4]
+print(max(list1))
+
+list2=[5,3,2,1,4]
+print(min(list2))
+
+list3=[5,3,2,1,4]
+print(len(list3))
--- level-43/homework/homework.py
+++
@@ -1,2 +0,0 @@
-list=[5,3,2,1,4]
-print(sum(list))
