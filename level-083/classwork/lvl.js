1673586fdb1ac3a7a309aa0e60bf6428389e9420
unknown <xtinctruler@gmail.com>
2025-04-10 01:26:24 -0700

    second commit

---
+++ level-083/classwork/lvl.js
@@ -0,0 +1,7 @@
+let sting="sting"
+let int= 5
+let float=2.3
+
+console.log(typeof string)
+console.log(typeof int)
+console.log(typeof float)
---
+++ level-085/cw/script.js
@@ -0,0 +1,11 @@
+
+let count =0
+ function counter1(){
+    count += 69
+document.getElementById("counts").textContent=count
+ }
+
+ function counter2(){
+    count -= 69
+document.getElementById("counts").textContent=count
+ }
